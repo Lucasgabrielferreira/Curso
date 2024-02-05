1afde221b2329dfd05d9f4e3571e5972dbb1c9ed
Lucas Gabriel <lg139826065@gmail.com>
2024-02-05 17:17:24 -0300

    Classes e outros tipos

---
+++ ProjetoAulas/Aulas_Modulo_a_7.cs
@@ -0,0 +1,150 @@
+/*Console.WriteLine("Primeiro Programa!");
+
+int idade = 24;
+Console.WriteLine(idade);
+
+string nome = "Lucas";
+Console.WriteLine(nome);
+
+decimal valor = 200.99m;
+Console.WriteLine(valor);
+
+double valorDouble = 200.99;
+Console.WriteLine(valorDouble);
+
+float valorFloat = 200.99f;
+Console.WriteLine(valorFloat);
+
+var idadeNova = 32;
+Console.WriteLine(idadeNova);
+
+char flag = 'Y';
+Console.WriteLine(flag);
+
+bool ativo = true;
+Console.WriteLine(ativo);
+
+
+const string descricao = "Curso CSHARP";
+Console.WriteLine(descricao);*/
+
+//int numero1 = 1;
+//var numero2 = 2;
+
+/*int soma = numero1 + numero2;
+Console.WriteLine(soma);
+
+int subtracao = numero2 - numero1;
+Console.WriteLine(subtracao);
+
+int multiplicacao = (numero2 * numero1) * 10;
+Console.WriteLine(multiplicacao);
+
+int divisao = numero2 / numero1;
+Console.WriteLine(divisao);*/
+
+/*bool igual = numero1 == numero2;
+Console.WriteLine(igual);
+
+bool maior = numero2 > numero1;
+Console.WriteLine(maior);
+
+bool menor = numero2 < numero1;
+Console.WriteLine(menor);
+
+bool menorOuIgual = numero2 <= numero1;
+Console.WriteLine(menorOuIgual);
+
+bool maiorOuIgual = numero2 >= numero1;
+Console.WriteLine(maiorOuIgual);
+
+bool diferente = numero2 != numero1;
+Console.WriteLine(diferente);*/
+
+
+/*bool valido = numero2 > numero1 && 6 > 7;
+Console.WriteLine(valido);
+
+bool valido2 = numero2 > numero1 || 6 > 7;
+Console.WriteLine(valido2);
+
+bool valido3 = !(numero2 > 3);
+Console.WriteLine(valido3);*/
+
+/*bool ativo = false;
+string status = ativo == true ? "Cadastro Ativo" : "Cadastro Inativo";
+Console.WriteLine(status);*/
+
+
+
+/*EscreverNome();
+
+void EscreverNome()
+{
+    var nome = NomeCompleto();
+    var soma = SomaValores();
+
+    Console.WriteLine(nome);
+    Console.WriteLine(soma);
+}
+
+string NomeCompleto()
+{
+    string primeiroNome = "Lucas";
+    string segundoNome = "Gabriel";
+
+    return primeiroNome + " " + segundoNome;
+}
+
+int SomaValores()
+{  
+    return 8 + 2;
+} */
+
+
+/*var soma = SomaValores(3,5);
+Console.WriteLine(soma);
+
+var nome = NomeEIdade("Lucas", 24);
+Console.WriteLine(nome);
+
+int SomaValores(int a, int b)
+{
+    return a + b;
+}
+
+string NomeEIdade(string nome, int idade)
+{
+    return "Meu nome é" + nome + "e tenho " + idade + " anos.";
+}*/
+
+//Modulo 6 :
+/*using System.Collections;
+var arryList = new ArrayList();
+
+arryList.Add(1);
+arryList.Add("Lucas");
+arryList.Add(true);
+
+Console.WriteLine(arryList[1]);
+
+arryList.RemoveAt(1);
+
+arryList.Clear();
+foreach (var item in arryList)
+{
+    Console.WriteLine(item);
+}*/
+
+//Modulo 7 :
+/*var diaDaSemana = 0;
+var diaDeTrabalho = true;
+
+if(diaDaSemana == 0 && diaDeTrabalho)
+{
+    Console.WriteLine("Hoje é domigo");
+}
+else
+{
+    
+}*/
---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AulaClasses();      
+    }
+
+    private static void AulaClasses()
+    {
+          var resultado = Cadastro.Calculos.SomarNumeros(2,3);
+        Console.WriteLine(resultado);
+
+        /*var produto = new Cadastro.Produto();
+        produto.SetId(1);
+        produto.GetId();
+
+        produto.Descricao = "Teclado";
+
+
+        produto.ImprimirDescricao();
+        Console.WriteLine(produto.GetId());
+        */
+    }
+
+}
